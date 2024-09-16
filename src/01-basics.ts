4587833c6c8be1714829bbab40eaa0f54b69c448
Andrew Koval <andrewkoval@Andrews-MBP.wit.ie>
2024-09-16 16:14:44 +0100

    complete type safety to colleagues data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -12,6 +12,10 @@ interface Colleague{
     extension: number
   } 
 }
+interface ColleagueHistory{
+  current: Colleague[],
+  former: Colleague[]
+}
 
 const friend1 = {
   name: "Paul Fleming",
@@ -55,7 +59,7 @@ const colleague3 = {
     extension: 125,
   },
 };
-const colleagues = {
+export const colleagues : ColleagueHistory = {
   current: [colleague1, colleague2, colleague3],
   former: [],
 };
