46a560c5c8c70a275ecb2689ef67d3d1d3861eb9
Andrew Koval <andrewkoval@Andrews-MBP.wit.ie>
2024-09-16 16:36:21 +0100

    Refactor - Add decleration file; disable redundant console logs

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -1,21 +1,4 @@
-interface Friend{
-  name: string;
-  phone: string;
-  age: number
-}
-
-interface Colleague{
-  name: string;
-  department: string;
-  contact: {
-    email: string;
-    extension: number
-  } 
-}
-interface ColleagueHistory{
-  current: Colleague[],
-  former: Colleague[]
-}
+import { Friend, Colleague, ColleagueHistory } from "./myTypes";
 
 const friend1 = {
   name: "Paul Fleming",
@@ -29,8 +12,7 @@ const friend2 = {
   age: 31,
 };
 
-const friends = [friend1, friend2];
-console.log(friends[1]);
+export const friends = [friend1, friend2];
 
 //   -------------------
 const colleague1 = {
@@ -64,4 +46,3 @@ export const colleagues : ColleagueHistory = {
   former: [],
 };
 
-console.log(colleagues.current[0]);
